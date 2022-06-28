ca57588ba887f510ae14a68df88b13d9661135ae
cloche3 <henndoifu@gmail.com>
2022-06-28 16:10:42 +0900

    2022/06/28

--- RubikCage.py
+++ RubikCage.py
@@ -1,4 +1,6 @@
 import copy
+from hashlib import new
+from string import hexdigits
 import numpy as np
 
 
@@ -33,3 +35,71 @@ def nxst(self):
     hash_t = set()
     nxway = self.nxbl()
     for x,y in nxway:
+        child = copy.deepcopy(self)
+        size = 4
+        stage_low = []
+        for j in range(size):
+            if j == y:
+                low = []
+                for k in range(size):
+                    if k == x:
+                        pass # 未実装
+                    else:
+                        pass # 未実装
+            if len(stage_low) != 0:
+                pass # 未実装
+            else:
+                pass # 未実装
+        else:
+            if len(stage_low) != 0:
+                pass # 未実装
+            else:
+                pass # 未実装
+    child.heights = tuple(stage_low)
+
+def stcan(self):
+    st_dic = {}
+    new_stone = self.new_stone
+    s_array = self.heights
+
+    for i in range(4):
+
+        #転置
+        st_ins = stage(heights = s_array, new_stone = new_stone, depth = self.depth)
+        st_dic[st_ins.heights] = st_ins
+        s_array = tuple(tuple(s_array[i][j] for i in range(size)) for j in range(size))
+        new_stone = (new_stone[1], new_stone[0], new_stone[2])
+
+        #回転
+        st_ins = stage(heigits = s_array, new_stone = new_stone, depth = self.depth)
+        st_dic[st_ins.heights] = st_ins
+        s_array = tuple(l[::1] for l in s_array)
+        new_stone = (size-1-new_stone[0], new_stone[1], new_stone[2])
+
+    st_can = st_dic[min(st_dic)]
+    return st_can
+
+def st_ch(target):
+    children = target.nxst()
+    for i in children:
+        all_in = i.check_all()
+        if all_in == 1 and target.depth%2 == 0:
+            return 1
+        elif all_in == 2 and target.depth%2 == 1:
+            return 2
+        drow = 0
+    for i in children:
+        judge = st_ch(i)
+        if judge == 1 and target.depth % 2 == 0:
+            return 1
+        elif judge == 2 and target.depth % 2 == 1:
+            return 2
+        elif judge == 0 or len(children) == 0:
+            drow = 1
+        if drow == 1:
+            return 0
+    if target.depth%2 == 0:
+        return 2
+    else:
+        return 1
+
